7ae911f0e0849ce180ec51e8686a4c157e5119d2
Jessica <jessica@email.com>
2020-03-13 17:17:17 +0000

    Specific types and casting

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,3 @@
-let negociacao = new Negociacao(new Date(), 1, 100);
-console.log(negociacao);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+document.querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
